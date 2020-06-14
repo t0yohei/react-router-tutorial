5c770335ad66edf56e0b95fb01cc77594d9f01f5
t0yohei <inuneko690121@gmail.com>
2020-06-14 18:50:31 +0900

    react routerを適用してページ遷移を実施

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
-var debug   = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path    = require('path');
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
   context: path.join(__dirname, "src"),
   entry: "./js/client.js",
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env']
-        }
-      }]
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              plugins: ["react-html-attrs"],
+              presets: ["@babel/preset-react", "@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
   },
   output: {
     path: __dirname + "/src/",
     filename: "client.min.js",
-    publicPath: '/'
+    publicPath: "/",
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  plugins: debug
+    ? []
+    : [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          mangle: false,
+          sourcemap: false,
+        }),
+      ],
 };
